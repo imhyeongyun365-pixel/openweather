0afbdcd0f59ea32e41f525c36acb457e2d50c986
unknown <imhyeongyun365@gmail.com>
2026-02-11 13:50:14 +0900

    검색,검색창 입력시 사라지는 버그 미해결

--- src/data/bgColor.js
+++ src/data/bgColor.js
@@ -1,13 +1,13 @@
 import img1 from "../assets/img1.png"
-import img1 from "../assets/img2.png"
-import img1 from "../assets/img3.png"
-import img1 from "../assets/img4.png"
-import img1 from "../assets/img5.png"
-import img1 from "../assets/img6.png"
-import img1 from "../assets/img7.png"
+import img2 from "../assets/img2.png"
+import img3 from "../assets/img3.png"
+import img4 from "../assets/img4.png"
+import img5 from "../assets/img5.png"
+import img6 from "../assets/img6.png"
+import img7 from "../assets/img7.png"
 
 export const getColorByWeatherId=(weatherId)=>{
-    if(weatherId ===800) return '#aFCD34D'
+    if(weatherId ===800) return img1
     const group=Math.floor(weatherId/100)
 
     switch (group) {
